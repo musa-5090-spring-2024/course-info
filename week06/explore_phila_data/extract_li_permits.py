8a4c5023f8b4d6c416fa0740bbcf95310893e7c6
Mjumbe Poe <mjumbewu@gmail.com>
2024-02-28 15:35:47 -0500

    Update the URL for the permit data

--- week06/explore_phila_data/extract_li_permits.py
+++ week06/explore_phila_data/extract_li_permits.py
@@ -4,8 +4,8 @@ import requests
 DATA_DIR = pathlib.Path(__file__).parent / 'raw_data'
 
 # Download the L&I Permits data as a GeoPackage
-url = 'https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+business_licenses&filename=business_licenses&format=geojson&skipfields=cartodb_id'
-filename = DATA_DIR / 'business_licenses.geojson'
+url = 'https://phl.carto.com/api/v2/sql?filename=permits&format=gpkg&skipfields=cartodb_id&q=SELECT%20*%20FROM%20permits%20WHERE%20permitissuedate%20%3E=%20%272016-01-01%27'
+filename = DATA_DIR / 'phl_li_permits.gpkg'
 
 response = requests.get(url)
 response.raise_for_status()
