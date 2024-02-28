d5a9b5554661195de145e528431af24b741329ba
Mjumbe Poe <mjumbewu@gmail.com>
2024-02-28 11:39:28 -0500

    Update extract scripts to use relative paths in week 6

--- week06/explore_phila_data/extract_phl_opa_properties.mjs
+++ week06/explore_phila_data/extract_phl_opa_properties.mjs
@@ -1,8 +1,11 @@
 import fetch from 'node-fetch';
 import fs from 'fs/promises';
 
+const __dirname = new URL('.', import.meta.url).pathname;
+const DATA_DIR = __dirname + 'raw_data/';
+
 const url = 'https://opendata-downloads.s3.amazonaws.com/opa_properties_public.csv';
-const filename = 'phl_opa_properties.csv';
+const filename = DATA_DIR + 'phl_opa_properties.csv';
 
 const response = await fetch(url);
 if (!response.ok) {
