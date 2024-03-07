ff013f2f5cbe079fc450cc078de7e9c4055a7afd
Mjumbe Poe <mjumbewu@gmail.com>
2024-03-07 12:24:40 -0500

    Avoid loading the entire L&I permits geopackage into memory unnecessarily

--- week06/explore_phila_data/prepare_phl_li_permits.py
+++ week06/explore_phila_data/prepare_phl_li_permits.py
@@ -1,5 +1,6 @@
 import fiona
 import json
+import math
 import pathlib
 
 RAW_DATA_DIR = pathlib.Path(__file__).parent / 'raw_data'
@@ -8,15 +9,19 @@ PREPARED_DATA_DIR = pathlib.Path(__file__).parent / 'prepared_data'
 raw_filename = RAW_DATA_DIR / 'phl_li_permits.gpkg'
 prepared_filename = PREPARED_DATA_DIR / 'phl_li_permits.jsonl'
 
-# Load the data from the GeoPackage file
-with fiona.open(raw_filename, 'r') as source:
-    data = [feature for feature in source]
+with (
+    # Load the data from the GeoPackage file
+    fiona.open(raw_filename, 'r') as source,
 
-# Write the data to a JSONL file
-with open(prepared_filename, 'w') as f:
-    for feature in data:
-        row = dict(feature['properties'])
-        row['geog'] = json.dumps(dict(feature['geometry']))
+    # Write the data to a JSONL file
+    open(prepared_filename, 'w') as f
+):
+    for feature in source:
+        row = fiona.model.to_dict(feature.properties)
+        row['geog'] = (
+            None if any(math.isnan(c) for c in feature.geometry.coordinates)
+            else json.dumps(fiona.model.to_dict(feature.geometry))
+        )
         f.write(json.dumps(row) + '\n')
 
 print(f'Processed data into {prepared_filename}')
