120e108729993a30d892ba153e0d87e414b4eb01
Mjumbe Poe <mjumbewu@gmail.com>
2024-04-02 10:56:34 -0400

    Update the run_sql function in week08

--- week08/explore_phila_data/run_sql/index.mjs
+++ week08/explore_phila_data/run_sql/index.mjs
@@ -4,9 +4,9 @@ dotenv.config({ path: findConfig('.env') });
 
 import fs from 'fs/promises';
 import path from 'path';
+import process from 'process';
 import { fileURLToPath } from 'url';
 
-import process from 'process';
 import functions from '@google-cloud/functions-framework';
 import { BigQuery } from '@google-cloud/bigquery'
 
@@ -14,27 +14,46 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const SQL_DIR_NAME = path.join(__dirname, 'sql');
 
 
-functions.http('load_phl_opa_properties', async (req, res) => {
-  const bucketName = process.env.DATA_LAKE_BUCKET;
-  const datasetName = process.env.DATA_LAKE_DATASET;
-
+functions.http('run_sql', async (req, res) => {
   // Read SQL file specified in the request
   const sqlPath = path.join(SQL_DIR_NAME, req.query['sql']);
+
   // Check that the file exists
   try {
-    await fs.promises.access(sqlPath);
+    await fs.access(sqlPath);
   } catch (err) {
     // If not, return a 404
-    res.status(404).send(`SQL file ${sqlPath} not found`);
+    res.status(404).send(`SQL file ${sqlPath} not found: ${err}`);
     return;
   }
 
-  const sqlQuery = await fs.readFile(sqlPath, 'utf8');
+  // Read the SQL file
+  const sqlQueryTemplate = await fs.readFile(sqlPath, 'utf8');
+  const sqlQuery = renderTemplate(
+    sqlQueryTemplate,
+    {
+      bucket_name: process.env.DATA_LAKE_BUCKET,
+      dataset_name: process.env.DATA_LAKE_DATASET,
+    },
+  )
 
   // Run the query
   const bigqueryClient = new BigQuery();
-  await bigqueryClient.query(eval(sqlQuery.replace(/`/g, '\\`')));
+  await bigqueryClient.query(sqlQuery);
 
   console.log(`Ran the SQL file ${sqlPath}`);
   res.send(`Ran the SQL file ${sqlPath}`);
 });
+
+
+function renderTemplate(sqlQueryTemplate, context) {
+  const cleanTemplate = sqlQueryTemplate.replace(/`/g, '\\`')
+  return eval(`
+    (function() {
+      ${Object.entries(context).map(
+        ([key, value]) => `const ${key} = '${value}';`
+      ).join('\n')}
+      return \`${cleanTemplate}\`;
+    })()`
+  );
+}
--- week08/explore_phila_data/run_sql/main.py
+++ week08/explore_phila_data/run_sql/main.py
@@ -10,24 +10,35 @@ DIR_NAME = pathlib.Path(__file__).parent
 SQL_DIR_NAME = DIR_NAME / 'sql'
 
 
-@functions_framework.http('GET')
-def load_phl_opa_properties(request):
-    bucket_name = os.getenv('DATA_LAKE_BUCKET')
-    dataset_name = os.getenv('DATA_LAKE_DATASET')
-
+@functions_framework.http
+def run_sql(request):
     # Read the SQL file specified in the request
     sql_path = SQL_DIR_NAME / request.args.get('sql')
+
     # Check that the file exists
     if (not sql_path.exists()) or (not sql_path.is_file()):
         # Return a 404 (not found) response if not
         return f'File {sql_path} not found', 404
 
-    with open(sql_path, 'r') as f:
-        sql_query = f.read(encoding='utf-8')
+    # Read the SQL file
+    with open(sql_path, 'r', encoding='utf-8') as sql_file:
+        sql_query_template = sql_file.read()
+        sql_query = render_template(
+            sql_query_template,
+            {
+                'bucket_name': os.getenv('DATA_LAKE_BUCKET'),
+                'dataset_name': os.getenv('DATA_LAKE_DATASET'),
+            }
+        )
 
     # Run the SQL query
     bigquery_client = bigquery.Client()
-    bigquery_client.query_and_wait(sql_query.replace('${', '{').format(dataset_name=dataset_name, bucket_name=bucket_name))
+    bigquery_client.query_and_wait(sql_query)
 
     print(f'Ran the SQL file {sql_path}')
     return f'Ran the SQL file {sql_path}'
+
+
+def render_template(sql_query_template, context):
+    clean_template = sql_query_template.replace('${', '{')
+    return clean_template.format(**context)
