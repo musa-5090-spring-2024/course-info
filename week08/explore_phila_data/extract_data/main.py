42eecec2b7f569607d7c5aa21f69524ef71b6f5a
Mjumbe Poe <mjumbewu@gmail.com>
2024-05-25 17:16:52 -0400

    Final changes from the semester

--- week08/explore_phila_data/extract_data/main.py
+++ week08/explore_phila_data/extract_data/main.py
@@ -8,16 +8,10 @@ import functions_framework
 from google.cloud import storage
 
 DIRNAME = pathlib.Path(__file__).parent
+BUCKET_NAME = os.getenv('DATA_LAKE_BUCKET')
 
 
-@functions_framework.http
-def extract_phl_opa_properties(request):
-    print('Extracting OPA Properties data...')
-
-    # Download the OPA Properties data as a CSV
-    url = 'https://opendata-downloads.s3.amazonaws.com/opa_properties_public.csv'
-    filename = DIRNAME / 'phl_opa_properties.csv'
-
+def extract_data(url, filename, blobname):
     response = requests.get(url)
     response.raise_for_status()
 
@@ -27,9 +21,6 @@ def extract_phl_opa_properties(request):
     print(f'Downloaded {filename}')
 
     # Upload the downloaded file to cloud storage
-    BUCKET_NAME = os.getenv('DATA_LAKE_BUCKET')
-    blobname = 'raw/phl_opa_properties/phl_opa_properties.csv'
-
     storage_client = storage.Client()
     bucket = storage_client.bucket(BUCKET_NAME)
     blob = bucket.blob(blobname)
@@ -37,4 +28,24 @@ def extract_phl_opa_properties(request):
 
     print(f'Uploaded {blobname} to {BUCKET_NAME}')
 
+
+@functions_framework.http
+def extract_phl_opa_properties(request):
+    print('Extracting OPA Properties data...')
+    extract_data(
+        'https://opendata-downloads.s3.amazonaws.com/opa_properties_public.csv',
+        DIRNAME / 'phl_opa_properties.csv',
+        'raw/phl_opa_properties/phl_opa_properties.csv',
+    )
+    return f'Downloaded to {filename} and uploaded to gs://{BUCKET_NAME}/{blobname}'
+
+
+@functions_framework.http
+def extract_phl_pwd_parcels(request):
+    print('Extracting PWD Parcels data...')
+    extract_data(
+        'https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+li_parcels&format=gpkg',
+        DIRNAME / 'phl_pwd_parcels.gpkg',
+        'raw/phl_pwd_parcels/phl_pwd_parcels.gpkg',
+    )
     return f'Downloaded to {filename} and uploaded to gs://{BUCKET_NAME}/{blobname}'
